ca89cb0c22060f1928ccd0b7d620b540fb8391cb
jpedro1711 <joaopedroigeski@gmail.com>
2025-03-17 20:19:24 -0300

    change car to be class and using webstorm

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,10 +1,18 @@
 // better use constructor and class
-
-export interface Car {
+export class Car{
   id?: number;
   model: string;
   brand: string;
   created_at?: Date;
   updated_at?: Date;
-  user_id: number
+  user_id: number;
+
+  constructor(id: number, model: string, brand: string, user_id: number, created_at?: Date, updated_at?: Date) {
+    this.id = id;
+    this.model = model;
+    this.brand = brand;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.user_id = user_id;
+  }
 }
